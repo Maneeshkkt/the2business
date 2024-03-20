3df8632a10fd483d542af6364a8295a76dd60af3
Maneeshkkt <141389961+Maneeshkkt@users.noreply.github.com>
2024-03-20 23:36:06 +0530

    Update resp.js

--- resp.js
+++ resp.js
@@ -32,3 +32,43 @@ function sentToWhatsapp(){
     window.open(url, '_blank').focus();
 
 }
+
+
+function emailSend(){
+    var emailMr=document.getElementById('email2').value;
+    var userName=document.getElementById('name2').value;
+    var texArea=document.getElementById('textarea2').value;
+    
+    var messageBody="Email: " + emailMr +
+    "<br> Name: " + userName +
+    "<br/> Meessage: " + texArea;
+
+Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "maneeshthinker@gmail.com",
+    Password : "46D87C990221CA3D20EDDBFCC47932327803",
+    To : 'maneeshthinker2@gmail.com',
+    From : "maneeshthinker@gmail.com",
+    Subject : "This is the subject",
+    Body : messageBody,
+}).then(
+  message => {
+    if(message=='OK'){
+        swal( "success",
+             "You clicked the button!",
+             "success",
+        );
+            
+          }
+          else{
+            swal(
+                "faild",
+                "You clicked the button!",
+                "somthing wrong",
+                
+              );
+          
+    }
+  }
+);
+}
