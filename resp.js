f66224c598f5611f00f594432f828343b9d40339
Maneeshkkt <141389961+Maneeshkkt@users.noreply.github.com>
2024-03-20 23:14:26 +0530

    Update resp.js

--- resp.js
+++ resp.js
@@ -12,3 +12,23 @@ burger.addEventListener('click', ()=>{
 
 
 })
+
+function sentToWhatsapp(){
+    let number="+918957118936";
+
+    let name=document.getElementById('name2').value;
+    let email=document.getElementById('email2').value;
+    let filep=document.getElementById('file2').value;
+    let meesage=document.getElementById('textarea2').value;
+    
+
+    var url="https://wa.me/" + number + "?text="
+    +"Name : " +name+ "%0a"
+    +"Email : " +email+ "%0a"
+    +"Plan : " +filep+ "%0a"
+    +"Message : " +meesage+ "%0a%0a";
+    
+
+    window.open(url, '_blank').focus();
+
+}
